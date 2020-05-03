fe57bd715f3ffe659deaaea7c70425520afccf9c
nguyenthanhhoang21095 <thanhhoang021095@gmail.com>
2020-05-03 09:03:24 +0700

    first lession

---
+++ week1/index.js
@@ -0,0 +1,46 @@
+const students = [
+  {
+    name: "Nam",
+    age: 24,
+    gender: "male",
+  },
+  {
+    name: "Mai",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "Trang",
+    age: 23,
+    gender: "female",
+  },
+  {
+    name: "An",
+    age: 20,
+    gender: "male",
+  },
+  {
+    name: "Thien",
+    age: 27,
+    gender: "male",
+  },
+];
+
+// Count gender
+function groupBy(property) {
+    return students.reduce((genderObj, student) => {
+    var key = student[property];
+    if (!genderObj[key]) {
+      genderObj[key] = [];
+    }
+    genderObj[key].push(student);
+    return genderObj;
+  }, {})
+};
+
+const genderGroup = groupBy('gender')
+console.log(genderGroup);
+
+// Student Name
+const nameList = students.map(student => student.name);
+console.log(nameList);
