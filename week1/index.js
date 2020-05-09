f908ec8532b8a57e242d2cf746598e02eae6accb
nguyenthanhhoang21095 <thanhhoang021095@gmail.com>
2020-05-09 13:37:45 +0700

    update fix homework

--- week1/index.js
+++ week1/index.js
@@ -26,23 +26,64 @@ const students = [
   },
 ];
 
+// ================= prototype.reduce ===========================
 // Count gender
-function groupBy(property) {
-    return students.reduce((genderObj, student) => {
-    var key = student[property];
-    if (!genderObj[key]) {
-      genderObj[key] = [];
-    }
-    genderObj[key].push(student);
-    return genderObj;
-  }, {})
+// function groupBy(property) {
+//     return students.reduce((genderObj, student) => {
+//     var key = student[property];
+//     if (!genderObj[key]) {
+//       genderObj[key] = [];
+//     }
+//     genderObj[key].push(student);
+//     return genderObj;
+//   }, {})
+// };
+
+// const genderGroup = groupBy('gender')
+// console.log("male:", genderGroup.male.length);
+// console.log("female:", genderGroup.female.length);
+
+// ================= for-loop ===========================
+// fix
+
+// const doCount = (elm, result, type) => {};
+
+const reduce = (arr, callback, result = {}) => {
+  for (let i = 0; i < arr.length; i++) {
+    result = callback(i, arr[i], result);
+  }
+  return result;
 };
 
-const genderGroup = groupBy('gender')
-console.log("male:", genderGroup.male.length);
-console.log("female:", genderGroup.female.length);
+const countByType = (type) => {
+  return reduce(students, function (idx, value, result) {
+    const count = result[value[type]];
+    result[value[type]] = (count || 0) + 1;
+    return result;
+  });
+};
+// const callback = (idx, value, result = {}, arr) => {
+//   const countByType = result[elm[type]];
+//   result[elm[type]] = (countByType || 0) + 1;
+//   return result;
+// };
+console.log("for loop",countByType("gender"));
 
+// ================= for-loop ===========================
+const countByRecuresive = (arr, type, result = {}, idx = 0) => {
+  if (idx > arr.length -1) {
+    return result;
+  }
+  const elm = arr[idx];
+  const count = result[elm[type]];
+  result[elm[type]] = (count || 0) + 1;
+  return result;
+
+  countByRecuresive(arr, type, result, idx + 1);
+}
+
+console.log("recursive",countByRecuresive(students, "gender"));
 
 // Student Name
-const nameList = students.map(student => student.name);
+const nameList = students.map((student) => student.name);
 console.log(nameList);
