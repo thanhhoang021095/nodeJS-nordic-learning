295d506b712952cb1787c87f290a0e8a9ec67246
nguyenthanhhoang21095 <thanhhoang021095@gmail.com>
2020-06-20 17:12:31 +0700

    edit home page

--- project/routes/index.router.js
+++ project/routes/index.router.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const users = require('../data/user.json');
+const products = require('../data/product.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    users: users.body,
+    products: products.body
+  });
 });
 
 module.exports = router;
